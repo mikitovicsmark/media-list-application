6590b3cfece8e966965e1139c4caa1128f091b50
Mark Mikitovics <mikitovicsm@gmail.com>
2017-01-14 20:30:21 +0100

    moving success function after api call

--- js/app/model/mainModel.js
+++ js/app/model/mainModel.js
@@ -8,20 +8,29 @@ define(['jquery'], ($) => {
       $.ajax({
         url: "http://146.185.158.18/fake_api.php",
         dataType: "jsonp",
-        success: function(response) {
-          console.log(response);
-          $.each(response, (index, val) => {
-            let elementId = `video-${val.id}`;
-            $('#video-container').append($('<div/>').attr('id', elementId));
-            $(`#${elementId}`).text(JSON.stringify(val));
-            $('#video-container').append($('<hr/>'));
-          });
-        },
-        error: function() {
-          console.log("something went wrong");
-        }
+        success: this.getDataSuccess.bind(this),
+        error: this.getDataError.bind(this)
       });
     }
+
+    getDataSuccess(response) {
+      $.each(response, (index, val) => {
+        this.createVideoElement(index, val);
+      });
+    }
+
+    getDataError(error) {
+      console.log(error);
+    }
+
+    createVideoElement(index, value) {
+      let elementId = `video-${value.id}`;
+      $('#video-container').append(`<div id="${elementId}"/>`);
+      $(`#${elementId}`).append(`<div class="video-title"> ${value.title} </div>`);
+      $(`#${elementId}`).append(`<img class="video-image" src="${value.picture}"/>`);
+      $('#video-container').append($('<hr/>'));
+    }
+
   }
 
   return new MainModel;
